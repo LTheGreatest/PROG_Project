59ea651c7d7171aa53e9ce60960e090b5a92db18
LTheGreatest <131713822+LTheGreatest@users.noreply.github.com>
2023-04-24 21:41:07 +0100

    Update Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -1,20 +1,28 @@
+#include "Color.hpp"
+
+
 Color::Color(unsigned char red, unsigned char green, unsigned char blue){
         red_ = red;
         green_ = green;
         blue_ = blue;
 }
+
 Color::Color(const Color& c) : red_(c.red_), green_(c.green_), blue_(c.blue_){}
+
 const Color Color::RED(255,0,0);
 const Color Color::GREEN(0,255,0);
 const Color Color::BLUE(0,0,255);
 const Color Color::BLACK(0,0,0);
 const Color Color::WHITE(255,255,255);
+
 unsigned char Color::red() const{
     return red_;
 }
+
 unsigned char Color::green() const{
     return green_;
 }
+
 unsigned char Color::blue() const{
     return blue_;
 }
