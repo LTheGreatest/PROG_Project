46561e52809b0835b97e50a802896179d0d42d15
LTheGreatest <131713822+LTheGreatest@users.noreply.github.com>
2023-04-25 17:21:19 +0100

    Add files via upload

---
+++ Script.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <fstream>
+#include "Script.hpp"
+#include "PNG.hpp"
+#include "XPM2.hpp"
+
+using namespace std;
+
+namespace prog {
+    // Use to read color values from a script file.
+    istream& operator>>(istream& input, Color& c) {
+        int r, g, b;
+        input >> r >> g >> b;
+        c.red() = r;
+        c.green() = g;
+        c.blue() = b;
+        return input;
+    }
+
+    Script::Script(const string& filename) :
+            image(nullptr), input(filename) {
+
+    }
+    void Script::clear_image_if_any() {
+        if (image != nullptr) {
+            delete image;
+            image = nullptr;
+        }
+    }
+    Script::~Script() {
+        clear_image_if_any();
+    }
+
+    void Script::run() {
+        string command;
+        while (input >> command) {
+            cout << "Executing command '" << command << "' ..." << endl;
+            if (command == "open") {
+                open();
+                continue;
+            }
+            if (command == "blank") {
+                blank();
+                continue;
+            }
+            // Other commands require an image to be previously loaded.
+            if (command == "save") {
+                save();
+                continue;
+            } 
+            if (command == "invert"){
+                invert();
+                continue;
+            }
+
+        }
+    }
+    void Script::open() {
+        // Replace current image (if any) with image read from PNG file.
+        clear_image_if_any();
+        string filename;
+        input >> filename;
+        image = loadFromPNG(filename);
+    }
+    void Script::blank() {
+        // Replace current image (if any) with blank image.
+        clear_image_if_any();
+        int w, h;
+        Color fill;
+        input >> w >> h >> fill;
+        image = new Image(w, h, fill);
+    }
+    void Script::save() {
+        // Save current image to PNG file.
+        string filename;
+        input >> filename;
+        saveToPNG(filename, image);
+    }
+
+    void Script::invert() {
+        // Tansforms each individual pixel (r, g, b) to (255-r,255-g,255-b)
+        int height = image->height();
+        int width = image->width();
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                Color new_pixel = image->at(x, y);
+                new_pixel.blue() = 255 - new_pixel.blue();
+                new_pixel.red() = 255 - new_pixel.red();
+                new_pixel.green() = 255 - new_pixel.green();
+                image->at(x,y) = new_pixel;
+            }
+        }
+    }
+}
