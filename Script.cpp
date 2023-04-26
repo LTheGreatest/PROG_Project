f07b40016e55d14f70f4a2029c28a77b37418e6b
LTheGreatest <131713822+LTheGreatest@users.noreply.github.com>
2023-04-26 17:18:28 +0100

    Funções de alteração de imagem feitas

--- Script.cpp
+++ Script.cpp
@@ -47,7 +47,8 @@ namespace prog {
             if (command == "save") {
                 save();
                 continue;
-            } 
+            }
+            // Commands for simple image manipulations 
             if (command == "invert"){
                 invert();
                 continue;
@@ -56,10 +57,19 @@ namespace prog {
                 to_gray_scale();
                 continue;
             }
+            // Commands for dimension-changing operations
             if(command == "crop"){
                 int x, y, w, h;
                 input >> x >> y >> w >> h;
                 crop(x, y, w, h);
+                continue;
+            }
+            if(command == "rotate_left"){
+                rotate_left();
+                continue;
+            }
+            if(command == "rotate_right"){
+                rotate_right();
                 continue;
             }
         }
@@ -86,6 +96,8 @@ namespace prog {
         saveToPNG(filename, image);
     }
 
+    //Simple image manipulations (image dimensions are not altered) -----------------------------
+
     void Script::invert() {
         // Tansforms each individual pixel (r, g, b) to (255-r,255-g,255-b)
         int height = image->height();
@@ -102,8 +114,8 @@ namespace prog {
     }
 
     void Script::to_gray_scale(){
-        //Transforms each individual pixel (r, g, b) to (v, v, v)
-        //v = (r + g + b)/3
+        // Transforms each individual pixel (r, g, b) to (v, v, v)
+        // v = (r + g + b)/3
         int height = image->height();
         int width = image->width();
         for(int y = 0; y < height; y++){
@@ -119,16 +131,44 @@ namespace prog {
         }
     }
     
+
+    // Dimension-changing operations ---------------------------------------------------------
+
     void Script::crop(int x, int y, int w, int h){
-        //Reduce the image to all pixels contained in the rectangle
-        //Define by top-left corner (x,y), width w and height h
+        // Reduce the image to all pixels contained in the rectangle
+        // Define by top-left corner (x,y), width w and height h
          Image new_image(w ,h);
          for(int yy = y; yy < y+h; yy++){
              for(int xx = x; xx < x+w; xx++){
               new_image.at(xx-x,yy-y) = image->at(xx,yy);
-
             }
         }
         *image = new_image;
     }
+
+    void Script::rotate_left(){
+        // Rotate image right by 90 degrees
+        Image new_image(image->height(), image->width()); // height = current image width | width = curret image height
+        int h = new_image.height();
+        int w = new_image.width();
+        for(int x = 0; x < w; x++){
+            for(int y = 0; y < h; y++){
+                new_image.at(x,y) = image->at(h-y-1, x);
+            }
+        }
+        *image = new_image;
+    }
+
+    void Script::rotate_right(){
+        // Rotate image right by 90 degrees
+        Image new_image(image->height(), image->width()); // height = current image width | width = curret image height
+        int h = new_image.height();
+        int w = new_image.width();
+        for(int x = 0; x < w; x++){
+            for(int y = 0; y < h; y++){
+                new_image.at(x,y) = image->at(y, w-x-1);
+            }
+        }
+        *image = new_image;
+    }  
 }
