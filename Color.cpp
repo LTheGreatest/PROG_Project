5bac8f172e3121896205f8608c0e720404cd3f87
alvaroltorres <96893814+alvaroltorres@users.noreply.github.com>
2023-04-27 21:50:24 +0100

    Update Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -1,48 +1,47 @@
 #include "Color.hpp"
 
-namespace prog {
+namespace prog{
 
     //default contructor (default color is black)
-    Color::Color() {
+    Color::Color(){
         red_ = blue_ = green_ = 0;
     }
 
     //copy constructor
-    Color::Color(const Color& other) {
+    Color::Color(const Color& other){
         this->red_ = other.red_;
         this->blue_ = other.blue_;
         this->green_ = other.green_;
     }
 
     //constructor using supplied values
-    Color::Color(rgb_value red, rgb_value green, rgb_value blue) {
+    Color::Color(rgb_value red, rgb_value green, rgb_value blue){
         red_ = red;
         green_ = green;
         blue_ = blue;
     }
 
     //getters implementantion (red, green and blue)
-    rgb_value Color::red() const {
+    rgb_value Color::red() const{
         return red_;
     }
-    rgb_value Color::green() const {
+    rgb_value Color::green() const{
         return green_;
     }
-    rgb_value Color::blue() const {
+    rgb_value Color::blue() const{
         return blue_;
     }
 
     //mutable references to individual RGB color channels
-    rgb_value& Color::red()  {
+    rgb_value& Color::red(){
         return red_;
     }
-    rgb_value& Color::green()  {
+    rgb_value& Color::green(){
       return green_;
     }
-    rgb_value& Color::blue()  {
+    rgb_value& Color::blue(){
       return blue_;
     }
-
 
     //auxiliary variables 
     const Color Color::RED (255,0, 0);
