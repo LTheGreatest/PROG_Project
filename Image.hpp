13b18627021db3786c1ce9ca4039db6ba6a29160
alvaroltorres <96893814+alvaroltorres@users.noreply.github.com>
2023-04-27 21:54:58 +0100

    Update Image.hpp

--- Image.hpp
+++ Image.hpp
@@ -3,21 +3,21 @@
 #include "Color.hpp"
 #include <vector>
 
-namespace prog
-{
-  class Image
-  {
-  private:
-    int width_, height_;
-    std::vector<std::vector<Color>> pixels_;  //2d vector represents the pixels in the image
-  public:
-    Image(int w, int h, const Color &fill = {255, 255, 255});
-    ~Image(); //destructor
-    //getters
-    int width() const;
-    int height() const;
-    Color &at(int x, int y);
-    const Color &at(int x, int y) const;
+namespace prog{
+  
+  class Image{
+    
+    private:
+      int width_, height_;
+      std::vector<std::vector<Color>> pixels_;  //2d vector represents the pixels in the image
+    public:
+      Image(int w, int h, const Color &fill = {255, 255, 255});
+      ~Image(); //destructor
+      //getters
+      int width() const;
+      int height() const;
+      Color &at(int x, int y);
+      const Color &at(int x, int y) const;
   };
 }
 
