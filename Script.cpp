43c16efe1bee422cd1d6ece0fe20f8d981e06463
alvaroltorres <96893814+alvaroltorres@users.noreply.github.com>
2023-04-27 21:48:11 +0100

    Update Script.cpp

--- Script.cpp
+++ Script.cpp
@@ -3,11 +3,10 @@
 #include "Script.hpp"
 #include "PNG.hpp"
 #include "XPM2.hpp"
-
 using namespace std;
 
 namespace prog {
-    // Use to read color values from a script file.
+    //used to read color values from a script file
     istream& operator>>(istream& input, Color& c) {
         int r, g, b;
         input >> r >> g >> b;
@@ -43,12 +42,12 @@ namespace prog {
                 blank();
                 continue;
             }
-            // Other commands require an image to be previously loaded.
+            //other commands require an image to be previously loaded
             if (command == "save") {
                 save();
                 continue;
             }
-            // Commands for simple image manipulations 
+            //commands for simple image manipulations 
             if (command == "invert"){
                 invert();
                 continue;
@@ -57,7 +56,7 @@ namespace prog {
                 to_gray_scale();
                 continue;
             }
-            // Commands for dimension-changing operations
+            //commands for dimension-changing operations
             if(command == "crop"){
                 int x, y, w, h;
                 input >> x >> y >> w >> h;
@@ -75,14 +74,14 @@ namespace prog {
         }
     }
     void Script::open() {
-        // Replace current image (if any) with image read from PNG file.
+        //replace current image (if any) with image read from PNG file
         clear_image_if_any();
         string filename;
         input >> filename;
         image = loadFromPNG(filename);
     }
     void Script::blank() {
-        // Replace current image (if any) with blank image.
+        //replace current image (if any) with blank image
         clear_image_if_any();
         int w, h;
         Color fill;
@@ -90,70 +89,70 @@ namespace prog {
         image = new Image(w, h, fill);
     }
     void Script::save() {
-        // Save current image to PNG file.
+        //save current image to PNG file
         string filename;
         input >> filename;
         saveToPNG(filename, image);
     }
 
-    //Simple image manipulations (image dimensions are not altered) -----------------------------
+    //simple image manipulations (image dimensions are not altered): -----------
 
     void Script::invert() {
-        // Tansforms each individual pixel (r, g, b) to (255-r,255-g,255-b)
+        //transforms each individual pixel (r, g, b) to (255 - r, 255 - g, 255 - b)
         int height = image->height();
         int width = image->width();
-        for(int y = 0; y < height; y++){
-            for(int x = 0; x < width; x++){
+        for (int y = 0; y < height; y++){
+            for (int x = 0; x < width; x++){
                 Color new_pixel = image->at(x, y);
                 new_pixel.blue() = 255 - new_pixel.blue();
                 new_pixel.red() = 255 - new_pixel.red();
                 new_pixel.green() = 255 - new_pixel.green();
-                image->at(x,y) = new_pixel;
+                image->at(x, y) = new_pixel;
             }
         }
     }
 
     void Script::to_gray_scale(){
-        // Transforms each individual pixel (r, g, b) to (v, v, v)
-        // v = (r + g + b)/3
+        //transforms each individual pixel (r, g, b) to (v, v, v)
+        //v = (r + g + b) / 3
         int height = image->height();
         int width = image->width();
-        for(int y = 0; y < height; y++){
-            for(int x = 0; x < width; x++){
-                int blue = image->at(x,y).blue();
-                int red = image->at(x,y).red();
-                int green = image->at(x,y).green();
-                int v = (blue + green + red)/3;
-                image->at(x,y).blue() = v;
-                image->at(x,y).red() = v;
-                image->at(x,y).green() = v;
+        for (int y = 0; y < height; y++){
+            for (int x = 0; x < width; x++){
+                int blue = image->at(x, y).blue();
+                int red = image->at(x, y).red();
+                int green = image->at(x, y).green();
+                int v = (blue + green + red) / 3;
+                image->at(x, y).blue() = v;
+                image->at(x, y).red() = v;
+                image->at(x, y).green() = v;
             }
         }
     }
     
 
-    // Dimension-changing operations ---------------------------------------------------------
+    //dimension-changing operations: -----------
 
     void Script::crop(int x, int y, int w, int h){
-        // Reduce the image to all pixels contained in the rectangle
-        // Define by top-left corner (x,y), width w and height h
+        //reduce the image to all pixels contained in the rectangle
+        //define by top-left corner (x, y), width w and height h
          Image new_image(w ,h);
-         for(int yy = y; yy < y+h; yy++){
-             for(int xx = x; xx < x+w; xx++){
-              new_image.at(xx-x,yy-y) = image->at(xx,yy);
+         for(int yy = y; yy < y + h; yy++){
+             for(int xx = x; xx < x + w; xx++){
+              new_image.at(xx - x,yy - y) = image->at(xx, yy);
             }
         }
         *image = new_image;
     }
 
     void Script::rotate_left(){
-        // Rotate image right by 90 degrees
-        Image new_image(image->height(), image->width()); // height = current image width | width = curret image height
+        //rotate image right by 90 degrees
+        Image new_image(image->height(), image->width()); //height = current image width | width = curret image height
         int h = new_image.height();
         int w = new_image.width();
         for(int x = 0; x < w; x++){
             for(int y = 0; y < h; y++){
-                new_image.at(x,y) = image->at(h-y-1, x);
+                new_image.at(x,y) = image->at(h-y-1, x); 
             }
         }
         *image = new_image;
@@ -161,12 +160,12 @@ namespace prog {
 
     void Script::rotate_right(){
         // Rotate image right by 90 degrees
-        Image new_image(image->height(), image->width()); // height = current image width | width = curret image height
+        Image new_image(image->height(), image->width()); //height = current image width | width = curret image height
         int h = new_image.height();
         int w = new_image.width();
         for(int x = 0; x < w; x++){
             for(int y = 0; y < h; y++){
-                new_image.at(x,y) = image->at(y, w-x-1);
+                new_image.at(x, y) = image->at(y, w - x - 1);
             }
         }
         *image = new_image;
