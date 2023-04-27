9426cb7da56669a27e2fb75e6e0cd85a62eedfb1
alvaroltorres <96893814+alvaroltorres@users.noreply.github.com>
2023-04-27 21:53:14 +0100

    Update Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -2,14 +2,13 @@
 #include <vector>
 using namespace std;
 
-namespace prog
-{
-  //Constructor -> image with width w, height h and all pixels set to color fill
-  Image::Image(int w, int h, const Color &fill)
-  { 
+namespace prog{
+  
+  //constructor -> image with width w, height h and all pixels set to color fill
+  Image::Image(int w, int h, const Color &fill){
+    
     width_ = w;
     height_ = h;
-
     for (int y = 0; y < height_; y ++){ //range of values of y needed
       vector< Color > new_line;
       for (int x = 0; x < width_; x++){ //range of values of x needed
@@ -20,31 +19,26 @@ namespace prog
 
   }
 
-  //Destructor -> not needed in this case explain why?????
-  Image::~Image()
-  {
+  //destructor -> not needed in this case explain why?????
+  Image::~Image(){
   }
 
   //getters' implementation (width and heigth)
-  int Image::width() const
-  {
+  int Image::width() const{
     return width_;
   }
-  int Image::height() const
-  {
+  int Image::height() const{
     return height_;
   }
 
   //mutable reference to the values of pixel (x,y)
-  Color& Image::at(int x, int y)
-  {
+  Color& Image::at(int x, int y){
     return pixels_[y][x];
   }
 
 
   //read-only reference to the value of pixel (x,y)
-  const Color& Image::at(int x, int y) const
-  {
+  const Color& Image::at(int x, int y) const{
     return pixels_[y][x];
   }
 }
