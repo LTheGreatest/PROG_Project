c0f0ea27b4dd3f10fbe42f3d00bc84ca84586b6b
Returnedft <131713571+Returnedft@users.noreply.github.com>
2023-05-08 21:09:34 +0100

    Create XPM2.hpp

---
+++ XPM2.hpp
@@ -0,0 +1,11 @@
+#ifndef __prog_XPM2_hpp__
+#define __prog_XPM2_hpp__
+
+#include <string>
+#include "Image.hpp"
+namespace prog {
+    Color hexatocolor(const std::string& hexadecimal);
+    Image* loadFromXPM2(const std::string &file);
+    void saveToXPM2(const std::string& file, const Image* img);
+}
+#endif
