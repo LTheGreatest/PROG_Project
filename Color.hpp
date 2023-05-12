9f1f17c8b9017383fe8fcaf16a5e077e0573945a
LTheGreatest <131713822+LTheGreatest@users.noreply.github.com>
2023-05-12 17:04:17 +0100

    Update Color.hpp

--- Color.hpp
+++ Color.hpp
@@ -12,7 +12,6 @@ namespace prog{
       Color(); //default constructor
       Color(const Color& c); //copy constructor
       Color(rgb_value r, rgb_value g, rgb_value b);
-      static const Color RED, GREEN, BLUE, BLACK, WHITE; //auxiliary variables
       //Getters for individual RGB color channels + mutable references for individual RGB color channels
       rgb_value red() const;
       rgb_value& red();
