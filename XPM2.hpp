e1d5a71ac18c5e07902ce7e486b74202653a6c6c
alvaroltorres <96893814+alvaroltorres@users.noreply.github.com>
2023-05-12 07:04:20 +0100

    Update XPM2.hpp

--- XPM2.hpp
+++ XPM2.hpp
@@ -7,6 +7,7 @@
 namespace prog {
     unsigned long value(const char c1);
     Color hexatocolor(const std::string& hexadecimal);
+    std::string colortohexa(const Color& rgb);
     Image* loadFromXPM2(const std::string& file);
     void saveToXPM2(const std::string& file, const Image* img);
 }
