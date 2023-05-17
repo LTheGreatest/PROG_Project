7f0d9469d979c6bae9e7b9a50a49601f194e2331
alvaroltorres <96893814+alvaroltorres@users.noreply.github.com>
2023-05-17 17:51:32 +0100

    Update Color.hpp

--- Color.hpp
+++ Color.hpp
@@ -9,10 +9,10 @@ namespace prog{
     private:
       rgb_value green_, blue_, red_;
     public:
-      Color(); //default constructor
-      Color(const Color& c); //copy constructor
+      Color(); 
+      Color(const Color& c);
       Color(rgb_value r, rgb_value g, rgb_value b);
-      //Getters for individual RGB color channels + mutable references for individual RGB color channels
+      //getters for individual RGB color channels + mutable references for individual RGB color channels
       rgb_value red() const;
       rgb_value& red();
       rgb_value green() const;
