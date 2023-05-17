eb2cf32a724e422c46a266fb5bd825f0edb22536
alvaroltorres <96893814+alvaroltorres@users.noreply.github.com>
2023-05-17 17:57:05 +0100

    Update Image.hpp

--- Image.hpp
+++ Image.hpp
@@ -10,13 +10,13 @@ namespace prog{
       int width_, height_;
       std::vector<std::vector<Color>> pixels_;  //2d vector represents the pixels in the image
     public:
-      Image(int w, int h, const Color &fill = {255, 255, 255}); //creation of image
+      Image(int w, int h, const Color& fill = {255, 255, 255}); //creation of image
       ~Image(); //destructor
       //getters
       int width() const;
       int height() const;
       Color &at(int x, int y); //mutable reference to the value of pixel (x, y)
-      const Color &at(int x, int y) const; //getter for read-only reference to the value of pixel (x, y)
+      const Color& at(int x, int y) const; //getter for read-only reference to the value of pixel (x, y)
   };
 }
 
